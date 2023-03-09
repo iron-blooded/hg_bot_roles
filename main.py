e5863998c1b48cf0dac12b152455e0e9d05c9f09
iron-blooded <ayss2@mail.ru>
2023-03-09 19:47:32 +0500

    fix

--- main.py
+++ main.py
@@ -539,7 +539,7 @@ async def online(interaction: discord.Interaction, invisible: bool = True):
 @tree_commands.command(name="clearall", description="Удаляет все не закрепленные сообщения", guild=discord.Object(id=guild_id))
 async def online(interaction: discord.Interaction):
     await interaction.response.defer(ephemeral=True)
-    if max([i.permissions.administrator for i in interaction.user.roles]):
+    if not max([i.permissions.administrator for i in interaction.user.roles]):
         return await interaction.followup.send('Вы не достойны')
     messages= await getLastMessages(interaction.channel_id, raw=True)
     if len(messages) >= 500:
